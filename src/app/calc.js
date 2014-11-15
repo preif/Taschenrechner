3b60c0a06236eecc87e8853a4f1fd47ea5a5986f
preif <PReif@web.de>
2014-11-15 22:12:46 +0100

    div

--- src/app/calc.js
+++ src/app/calc.js
@@ -5,21 +5,37 @@
 angular.module ('calc', [])
     .controller ("display", ['$scope',
     function ($scope) {
+        $scope.editMode=function () {
+            $scope.status = 'edit';
+        }
+
         $scope.schreib = function (element) {
-            console.log ('schreib ', element);
+//            console.log ('schreib ', element);
             $scope.akku += element.toString ();
+            $scope.editMode ();
         };
         $scope.rechne = function () {
-            var result = $scope.$eval ($scope.akku);
-            if (angular.isDefined(result)) {
-                $scope.akku = result.toString();
-            } else {
-                console.log ('result is undefined');
+            var fehler = false;
+            try {
+                var result = $scope.$eval ($scope.akku);
+            } catch (error) {
+                fehler = true;
+                $scope.status = 'fehler';
+                console.log ('error: ', error)
+            }
+            if (!fehler) {
+                if (angular.isDefined(result)) {
+                    $scope.akku = result.toString();
+                    $scope.status = 'erfolg';
+                } else {
+                    console.log ('result is undefined');
+                }
             }
         };
         $scope.anzeige=function() {
             return $scope.akku.length ? $scope.akku : '0';
         };
         $scope.akku = '';
+        $scope.status = 'edit';
     }]);
 
